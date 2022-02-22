b0851a72af71fb73a3304a1067bbfde1913b3ac3
Shiwunan Cheng <shiwunancheng@gmail.com>
2022-02-21 20:29:37 -0500

    updated some stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GEN_IMG
- Generative string art based on images
+ Generative string art based on images.
+
+## USAGE
+  Change the source of the `img` tag to the location of the image that you want to process. Use the SVG Export extension to save the SVG.
--- gen.js
+++ gen.js
@@ -1,6 +1,5 @@
-var lnWt = 1;   //line weight
-var res = 2;    //number of pixels to skip
-
+var lnWt = 1; //line weight
+var res = 2; //number of pixels to skip
 
 // Get a reference to the image you want the pixels of and its dimensions
 var srcImg = document.getElementById("srcImg");
@@ -31,17 +30,41 @@ const out = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 out.setAttribute("width", w);
 out.setAttribute("height", h);
 
-for (let x = 0; x < w; x+=2) {
-  for (let y = 0; y < h; y+=2) {
-    if (data[(y*srcImg.width + x)*4+3] > 0) {
+for (let x = 0; x < w; x += res) {
+  for (let y = 0; y < h; y += res) {
+    if (data[(y * srcImg.width + x) * 4 + 3] > 0) {
       var newLine = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "line"
       );
       newLine.setAttribute("x1", x);
       newLine.setAttribute("y1", y);
-      newLine.setAttribute("x2", x+genRand(-50, 50));
-      newLine.setAttribute("y2", y+genRand(-50, 50));
+      if (
+        data[(y * srcImg.width + x) * 4 + 3] >
+        data[(y * srcImg.width + x + 1) * 4 + 3]
+      ) {
+        newLine.setAttribute("x2", x + genRand(-50, 0));
+      } else if (
+        data[(y * srcImg.width + x) * 4 + 3] >
+        data[(y * srcImg.width + x - 1) * 4 + 3]
+      ) {
+        newLine.setAttribute("x2", x + genRand(0, 50));
+      } else {
+        newLine.setAttribute("x2", x + genRand(-50, 50));
+      }
+      if (
+        data[(y * srcImg.width + x) * 4 + 3] >
+        data[((y + 1) * srcImg.width + x) * 4 + 3]
+      ) {
+        newLine.setAttribute("y2", y + genRand(-50, 0));
+      } else if (
+        data[(y * srcImg.width + x) * 4 + 3] >
+        data[((y - 1) * srcImg.width + x) * 4 + 3]
+      ) {
+        newLine.setAttribute("y2", y + genRand(0, 50));
+      } else {
+        newLine.setAttribute("y2", y + genRand(-50, 50));
+      }
       var hex =
         "#" +
         (
@@ -64,7 +87,7 @@ function rgbToHex(r, g, b) {
   return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-function genRand(min, max) {
+function genRand(min, max, x, t) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive

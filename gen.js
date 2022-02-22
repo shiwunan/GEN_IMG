553104581bb1750df7643447dea7c672be7ce20d
Shiwunan Cheng <shiwunancheng@gmail.com>
2022-02-21 23:49:09 -0500

    new stuff

--- gen.js
+++ gen.js
@@ -1,5 +1,5 @@
 var lnWt = 1; //line weight
-var res = 2; //number of pixels to skip
+var res = 3; //number of pixels to skip
 
 // Get a reference to the image you want the pixels of and its dimensions
 var srcImg = document.getElementById("srcImg");
@@ -30,16 +30,34 @@ const out = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 out.setAttribute("width", w);
 out.setAttribute("height", h);
 
+var isoPx_x = [];
+var isoPx_y = [];
+
 for (let x = 0; x < w; x += res) {
   for (let y = 0; y < h; y += res) {
-    if (data[(y * srcImg.width + x) * 4 + 3] > 0) {
+    if (
+      data[(y * srcImg.width + x) * 4 + 3] > 0 &&
+      ((x/res % 4 == 2 && y/res % 2 == 0) || (x/res % 4 == 0 && y/res % 2 != 0))
+    ) {
+      isoPx_x.push(x);
+      isoPx_y.push(y);
+    }
+  }
+}
+
+for (let x = 0; x < w; x += res) {
+  for (let y = 0; y < h; y += res) {
+    if (
+      data[(y * srcImg.width + x) * 4 + 3] > 0 &&
+      ((x/res % 4 == 2 && y/res % 2 == 0) || (x/res % 4 == 0 && y/res % 2 != 0))
+    ) {
       var newLine = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "line"
       );
       newLine.setAttribute("x1", x);
       newLine.setAttribute("y1", y);
-      if (
+      /* if (
         data[(y * srcImg.width + x) * 4 + 3] >
         data[(y * srcImg.width + x + 1) * 4 + 3]
       ) {
@@ -64,7 +82,12 @@ for (let x = 0; x < w; x += res) {
         newLine.setAttribute("y2", y + genRand(0, 50));
       } else {
         newLine.setAttribute("y2", y + genRand(-50, 50));
-      }
+      } */
+      newLine.setAttribute("x1", x);
+      newLine.setAttribute("y1", y);
+      var rnd = genRand(1, isoPx_x.length);
+      newLine.setAttribute("x2", isoPx_x[rnd]);
+      newLine.setAttribute("y2", isoPx_y[rnd]);
       var hex =
         "#" +
         (
@@ -87,7 +110,7 @@ function rgbToHex(r, g, b) {
   return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-function genRand(min, max, x, t) {
+function genRand(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
